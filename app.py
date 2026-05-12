6d8e2f92eaa9dedd14c68f7c4af4223ad7be9ad8
mudassirabbasi1 <123233715+mudassirabbasi1@users.noreply.github.com>
2026-05-12 14:56:34 +0500

    Full CRUD implementation: Create, Read, Update, Delete functionality

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    status = db.Column(db.Integer, default=0) # 0 for pending, 1 for completed
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -18,29 +19,37 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
+        if not task_content: return redirect('/')
         new_task = Todo(content=task_content)
-
         try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
+        except: return 'Issue adding task'
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
+    task = Todo.query.get_or_404(id)
     try:
-        db.session.delete(task_to_delete)
+        db.session.delete(task)
         db.session.commit()
         return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
+    except: return 'Issue deleting task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except: return 'Issue updating task'
+    else:
+        return render_template('update.html', task=task)
 
 if __name__ == "__main__":
     with app.app_context():
